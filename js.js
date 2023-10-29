dacf669312209057e765741648461249ed4b81db
Mojde7th <mojde7th@gmail.com>
2023-10-29 12:59:28 +0330

    menu10

---
+++ js.js
@@ -0,0 +1,19 @@
+// dollar
+<div class="PrimarySelect_options__30565">
+  <div class="PrimarySelect_option__3ErQe">
+    <span class="PrimarySelect_name__1FZKE">دلار</span>
+  </div>
+  <div class="PrimarySelect_option__3ErQe">
+    <span class="PrimarySelect_name__1FZKE">یورو</span>
+  </div>
+</div>;
+//jensiat
+
+<div class="PrimarySelect_options__30565">
+  <div class="PrimarySelect_option__3ErQe">
+    <span class="PrimarySelect_name__1FZKE">مذکر</span>
+  </div>
+  <div class="PrimarySelect_option__3ErQe">
+    <span class="PrimarySelect_name__1FZKE">مونث</span>
+  </div>
+</div>;
