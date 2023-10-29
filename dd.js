f0e3a8217a131e78871e0003f07828e7ad9242bc
Mojde7th <mojde7th@gmail.com>
2023-10-29 15:10:14 +0330

    Div vasat

---
+++ dd.js
@@ -0,0 +1,10 @@
+const spans = document.querySelectorAll(
+  ".PrimarySelect_options__30565 .PrimarySelect_name__1FZKE"
+);
+
+if (spans.length > 0) {
+  const firstSpan = spans[0];
+  console.log("jensiat:", firstSpan);
+
+  firstSpan.click();
+}
